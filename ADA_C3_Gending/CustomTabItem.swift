9f0739d9b70e08e202a6c61b7fccab9bfb14e37c
wikananda <nandasantana276@gmail.com>
2025-07-09 14:54:58 +0800

    restructure custom tab bar

--- ADA_C3_Gending/CustomTabItem.swift
+++ ADA_C3_Gending/CustomTabItem.swift
@@ -7,6 +7,41 @@
 
 import SwiftUI
 
+struct CustomTabBar: View {
+    @Binding var selectedIndex: Int
+    
+    var body: some View {
+        HStack (alignment: .center) {
+            CustomTabItem(iconName: "house", title: "Home", index: 0, selectedIndex: $selectedIndex)
+            Spacer()
+            CustomTabItem(iconName: "magnifyingglass", title: "Search", index: 1, selectedIndex: $selectedIndex)
+            Spacer()
+            CustomTabItem(iconName: "rectangle.stack", title: "My Library", index: 2, selectedIndex: $selectedIndex)
+        }
+//            .padding(.top, 20)
+        .padding(.horizontal, 50)
+        .frame(height: 80)
+//            .background(.dark1)
+        // Glassmorphism custom tab bar
+        .background {
+            ZStack {
+                Rectangle()
+                    .foregroundStyle(.ultraThinMaterial)
+                Rectangle()
+                    .foregroundStyle(.dark2.opacity(0.5))
+            }
+        }
+        .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: -5)
+        // add white line on top of tab bar
+        .overlay(
+            Rectangle()
+                .frame(height: 0.5)
+                .foregroundColor(.text1.opacity(0.3)),
+            alignment: .top
+        )
+    }
+}
+
 struct CustomTabItem: View {
     let iconName: String
     let title: String
@@ -36,5 +71,5 @@ struct CustomTabItem: View {
 
 #Preview {
     @State var selectedIndex = 0
-    CustomTabItem(iconName: "house", title: "Home", index: 0, selectedIndex: $selectedIndex)
+    CustomTabBar(selectedIndex: $selectedIndex)
 }
--- ADA_C3_Gending/SuaragamView.swift
+++ ADA_C3_Gending/SuaragamView.swift
@@ -26,35 +26,8 @@ struct SuaragamView: View {
             .background(.dark2)
             .foregroundColor(.text1)
             
-            // Custom tab bar
-            HStack (alignment: .center) {
-                CustomTabItem(iconName: "house", title: "Home", index: 0, selectedIndex: $selectedIndex)
-                Spacer()
-                CustomTabItem(iconName: "magnifyingglass", title: "Search", index: 1, selectedIndex: $selectedIndex)
-                Spacer()
-                CustomTabItem(iconName: "rectangle.stack", title: "My Library", index: 2, selectedIndex: $selectedIndex)
-            }
-//            .padding(.top, 20)
-            .padding(.horizontal, 50)
-            .frame(height: 80)
-//            .background(.dark1)
-            // Glassmorphism custom tab bar
-            .background {
-                ZStack {
-                    Rectangle()
-                        .foregroundStyle(.ultraThinMaterial)
-                    Rectangle()
-                        .foregroundStyle(.dark2.opacity(0.5))
-                }
-            }
-            .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: -5)
-            // add white line on top of tab bar
-            .overlay(
-                Rectangle()
-                    .frame(height: 0.5)
-                    .foregroundColor(.text1.opacity(0.3)),
-                alignment: .top
-            )
+            // Custom Tab Bar
+            CustomTabBar(selectedIndex: $selectedIndex)
         }
         .ignoresSafeArea()
     }
